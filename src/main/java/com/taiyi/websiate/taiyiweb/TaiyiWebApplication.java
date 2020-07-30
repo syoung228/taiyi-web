75e92811d77b2b853315fe12bafc015492c5b619
syoung <syoung228@163.com>
2020-07-30 15:38:27 +0800

    Initial commit

---
+++ src/main/java/com/taiyi/websiate/taiyiweb/TaiyiWebApplication.java
@@ -0,0 +1,13 @@
+package com.taiyi.websiate.taiyiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiyiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaiyiWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taiyi/websiate/taiyiweb/TaiyiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiyi.websiate.taiyiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiyiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
