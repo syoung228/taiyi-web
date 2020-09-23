8a4b15d3161c389b55f9b623172bbb800e17e1b0
syoung <syoung228@163.com>
2020-09-23 10:07:44 +0800

    优化静态化页面

--- src/main/java/com/taiyi/websiate/taiyiweb/utils/TemplateStaticUtil.java
+++ src/main/java/com/taiyi/websiate/taiyiweb/utils/TemplateStaticUtil.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class TemplateStaticUtil {
 
     private static String rootPath = "/taiyi/html/";
 
-    public  static void urlToHtml(String url,String path) throws IOException {
+   /* public  static void urlToHtml(String url,String path) throws IOException {
         InputStream inputStream;//接收字节输入流
         InputStreamReader inputStreamReader;//将字节输入流转换成字符输入流
         BufferedReader bufferedReader;//为字符输入流加缓冲
@@ -32,6 +33,23 @@ public class TemplateStaticUtil {
         bufferedReader.close();
         inputStreamReader.close();
         inputStream.close();
+    }*/
+
+    public static void urlToHtml(String urlString,String path) throws IOException {
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        //获得字节流
+        InputStream inputStream = connection.getInputStream();
+        //获得字节缓冲流
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        FileOutputStream fileOutputStream = new FileOutputStream(rootPath+path);
+        byte[] bytes = new byte[255];
+        int total_number;
+        while((total_number = bufferedInputStream.read(bytes)) != -1) {
+            //写入文件
+            fileOutputStream.write(bytes);
+        }
+        fileOutputStream.close();
     }
 
 
